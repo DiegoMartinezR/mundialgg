768a0b8b5d997e5f7378f32410cd4df5e87fe112
Diego Martinez <diego.martinez.r@tecsup.edu.pe>
2018-06-25 22:27:01 -0500

    asd

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,12 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '2220195594875720',
 		'clientSecret': '8395718fa5d1e24bf50d9071e3b7421e',
-		'callbackURL': 'http://18.188.39.12:3000/auth/facebook/callback'
+		'callbackURL': 'http://localhost:80/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
 		'clientID': '782346418431-68q7vnm73as8sofffcb9dsvhj6r0h7eb.apps.googleusercontent.com',
 		'clientSecret': 'Z3P37zqQSqSIctUH3biOvu9G',
-		'callbackURL': 'http://18.188.39.12:3000/auth/google/callback'
+		'callbackURL': 'http://localhost:80/auth/google/callback'
 	}
 }
